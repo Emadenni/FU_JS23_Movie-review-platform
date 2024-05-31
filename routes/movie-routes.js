bcd03a7b1b49ee1274ef9bba4591caa73c619c31
Emadenni <emanyele.dng86@gmail.com>
2024-05-31 23:14:15 +0200

    implemented task05 succesfully, ready to review

--- routes/movie-routes.js
+++ routes/movie-routes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const movieController = require("../controllers/movie.controller");
 const { auth } = require("../middlewares/auth");
 const { authorizeAdmin } = require("../middlewares/adminAuth");
-    const router = express.Router();
+const router = express.Router();
 
-    router.post("/", auth, authorizeAdmin,   movieController.addMovie);
-    router.put("/:id",auth,  authorizeAdmin,   movieController.updateMovie);
+router.post("/", auth, authorizeAdmin, movieController.addMovie);
+router.put("/:id", auth, authorizeAdmin, movieController.updateMovie);
+router.delete("/:id", auth, authorizeAdmin, movieController.deleteMovie);
 
-    module.exports = router;
+module.exports = router;
