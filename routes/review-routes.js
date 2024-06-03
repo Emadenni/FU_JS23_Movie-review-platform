8109ddf914644faa33aba2d26e67bc521e41ef3a
Emadenni <emanyele.dng86@gmail.com>
2024-06-03 09:55:38 +0200

    Implemented task11 succesfully, ready to review

--- routes/review-routes.js
+++ routes/review-routes.js
@@ -8,4 +8,5 @@ const { isAuthor} = require("../middlewares/isAuthor")
 router.post("/", auth, validateFields(reviewController.requiredCreateReviewFields), reviewController.createReview);
 router.put("/:id",  auth,  isAuthor, validateFields(reviewController.requiredUpdateReviewFields),  reviewController.updateReview);
 router.delete("/:id", auth, isAuthor, reviewController.deleteReview)
+router.get("/:id",auth, reviewController.getReviewById )
 module.exports = router;
